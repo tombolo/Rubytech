21070041f7e3e465d7aa4f13af7a3735671bc5ef
tomboloski <ndanumumo93@gmail.com>
2023-06-21 02:43:50 +0300

    day 8

--- src/components/FixedFooter.js
+++ src/components/FixedFooter.js
@@ -9,7 +9,7 @@ const Footer = () => {
   };
 
   return (
-    <footer className="fixed bottom-0 left-0 w-full bg-gray-50 text-white text-center py-1 rounded z-50 border-t-2 border-white">
+    <footer className="fixed bottom-0 left-0 w-full bg-gray-50 text-white text-center z-50 border-t-2 border-white">
       <div className="flex items-center justify-start ml-6">
         <button
           className={`mr-2 focus:outline-none ${
@@ -17,10 +17,12 @@ const Footer = () => {
           }`}
           onClick={handleLikeClick}
         >
-          <HeartIcon className="w-10 h-10" />
+          <HeartIcon className="w-8 h-8 mt-2" />
+          <span className="text-xs">Wish</span>
         </button>
         <button className="mr-2 focus:outline-none text-blue-900">
-          <ChatIcon className="w-12 h-12" />
+          <ChatIcon className="w-8 h-8 mt-2" />
+          <span className="text-xs">Chat</span>
         </button>
         <button className="w-48 h-10 flex items-center justify-center bg-white text-gray-800 border-blue-900 border-2 rounded-full ml-7">
           <span className="w-1/2 bg-blue-900 text-white py-3 text-xs rounded-full">
