85849486259297bae72ff9ffadb44bf19c53faec
tomboloski <ndanumumo93@gmail.com>
2023-06-21 02:30:50 +0300

    day 8

--- src/components/FixedFooter.js
+++ src/components/FixedFooter.js
@@ -9,21 +9,21 @@ const Footer = () => {
   };
 
   return (
-    <footer className="fixed bottom-0 left-0 w-full bg-gray-300 text-white text-center py-2 rounded z-50 border-t-2 border-yellow-700">
+    <footer className="fixed bottom-0 left-0 w-full bg-gray-50 text-white text-center py-1 rounded z-50 border-t-2 border-white">
       <div className="flex items-center justify-start ml-6">
         <button
           className={`mr-2 focus:outline-none ${
-            isLiked ? "text-red-500" : "text-white"
+            isLiked ? "text-red-500" : "text-gray-400"
           }`}
           onClick={handleLikeClick}
         >
-          <HeartIcon className="w-12 h-12" />
+          <HeartIcon className="w-10 h-10" />
         </button>
-        <button className="mr-2 focus:outline-none text-blue-500">
+        <button className="mr-2 focus:outline-none text-blue-900">
           <ChatIcon className="w-12 h-12" />
         </button>
-        <button className="w-48 h-12 flex items-center justify-center bg-white text-gray-800 border-blue-900 border-2 rounded-full ml-7">
-          <span className="w-1/2 bg-blue-900 text-white py-4 text-xs rounded-full">
+        <button className="w-48 h-10 flex items-center justify-center bg-white text-gray-800 border-blue-900 border-2 rounded-full ml-7">
+          <span className="w-1/2 bg-blue-900 text-white py-3 text-xs rounded-full">
             Add to Cart
           </span>
           <span className="w-1/2 bg-white text-blue-900 text-xs rounded-full">
