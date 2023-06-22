f29e14b054e201b6236ce9b82e8a77538286a168
tomboloski <ndanumumo93@gmail.com>
2023-06-22 19:16:20 +0300

    day 10

--- src/components/Control.js
+++ src/components/Control.js
@@ -5,17 +5,17 @@ const Control = () => {
     <div className="flex flex-row justify-between lg:justify-evenly items-center bg-gray-100 p-4 rounded-lg mx-6 flex-grow">
       <div className="flex flex-col items-center">
         <TruckIcon className="h-6 w-6 md:h-12 md:w-12 text-blue-500" />
-        <p className="text-xs mt-1 md:text-sm">Free Delivery</p>
+        <p className="text-xs mt-1 md:text-sm text-blue-900 font-semibold">Free Delivery</p>
       </div>
 
       <div className="flex flex-col items-center">
         <CurrencyDollarIcon className="h-6 w-6 md:h-12 md:w-12 text-green-500" />
-        <p className="text-xs mt-1 md:text-sm">Affordable Products</p>
+        <p className="text-xs mt-1 md:text-sm text-blue-900 font-semibold">Affordable Products</p>
       </div>
 
       <div className="flex flex-col items-center">
         <ClockIcon className="h-6 w-6 md:h-12 md:w-12 text-purple-500" />
-        <p className="text-xs mt-1 md:text-sm">Online Support</p>
+        <p className="text-xs mt-1 md:text-sm text-blue-900 font-semibold">Online Support</p>
       </div>
     </div>
   );
