0abb99200903b85eefc37bcb6e9a0cf9758fbc13
tomboloski <ndanumumo93@gmail.com>
2023-06-22 14:48:04 +0300

    day 9

--- src/components/About.js
+++ src/components/About.js
@@ -3,14 +3,14 @@ import { LocationMarkerIcon, PhoneIcon, MailIcon } from '@heroicons/react/solid'
 
 function About() {
   return (
-    <div className="flex flex-col mx-0 bg-white rounded-md">
+    <div className="flex flex-col mx-3 bg-white rounded-md mt-4">
 
-        <div className="m-2 border-b-2 border-yellow-900">
+        <div className="m-2 border-b-2 border-gray-200">
             <h1 className="text-sm font-bold text-blue-900 p-1">About Us</h1>
             <p className="text-xs p-1">Rubytech Limited is a dealer of Cooking/Salad Oil ATMs, Milk ATMs, Water Purifiers, Food Grade Pumps, Automatic Bell Systems, Programmable Logic Controller, and Water Stations based in Buruburu Kenya.</p>
         </div>
 
-        <div className="m-2 border-b-2 border-yellow-900">
+        <div className="m-2 border-b-2 border-gray-200">
             <h1 className="text-xs font-bold text-blue-900 p-1">Quick Links</h1>
             <p className="text-xs p-1">Home</p>
             <p className="text-xs p-1">Our Services</p>
@@ -18,7 +18,7 @@ function About() {
             <p className="text-xs p-1">Buy Again</p>
         </div>
 
-        <div className="m-2 border-b-2 border-yellow-900">
+        <div className="m-2 border-b-2 border-gray-200">
             <h1 className="text-sm font-bold text-blue-900 p-1">Our Location & Contact Details</h1>
             <div className="flex flex-row p-1">
                 <LocationMarkerIcon className="h-6 w-6 text-red-500" />
@@ -27,16 +27,16 @@ function About() {
 
             <div className="flex flex-row p-1">
                 <PhoneIcon className="h-6 w-6 text-yellow-500" /> 
-                <p className="text-sm mx-2">Phone: +254757351475</p>
+                <p className="text-xs mx-2">Phone: +254757351475</p>
             </div>
 
             <div className="flex flex-row p-1">
                 <MailIcon className="h-6 w-6 text-indigo-500" />
-                <p className="text-sm mx-2">ndanumumo93@gmail.com</p>
+                <p className="text-xs mx-2">ndanumumo93@gmail.com</p>
             </div>
         </div>
 
-        <div className="m-2 border-b-2 border-yellow-900">
+        <div className="m-2 border-b-2 border-gray-200">
         <h1 className="text-sm font-bold text-blue-900 p-1">Business Hours</h1>
 
                     <div className="flex flex-row items-center justify-between p-1">
