6463c7ee7f05f009cb7d16f5fc3b441d45628ce7
tomboloski <ndanumumo93@gmail.com>
2023-06-24 04:52:29 +0300

    day 12

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -11,11 +11,7 @@ function Categories() {
 
   return (
     <div className="lg:mt-2 -mt-2 z-50 lg:flex flex-row items-center justify-evenly">
-                    <div data-aos="fade-right"
-                        data-aos-duration="2000"
-                        data-aos-once="false"
-                        data-aos-delay="100"
-                        data-aos-easing="ease-in-out" className="flex flex-row justify-evenly mx-1 mt-5 lg:mt-0 items-center">
+                    <div className="flex flex-row justify-evenly mx-1 mt-5 lg:mt-0 items-center">
                         <div className="flex flex-col items-center bg-white rounded-md p-2 lg:mx-4">
                         <AdjustmentsIcon className="h-8 w-8 lg:h-12 lg:w-12 text-blue-500" />
                         <p className="text-xs lg:text-sm">Filtration Systems</p>
@@ -30,11 +26,7 @@ function Categories() {
                         </div>
                     </div>
 
-                    <div data-aos="fade-left"
-                         data-aos-duration="2000"
-                         data-aos-once="false"
-                         data-aos-delay="100"
-                         data-aos-easing="ease-in-out" className="flex flex-row justify-evenly mx-1 mt-5 lg:mt-0 items-center">
+                    <div className="flex flex-row justify-evenly mx-1 mt-5 lg:mt-0 items-center">
                         <div className="flex flex-col items-center bg-white rounded-md p-2 lg:mx-4">
                                 <GlobeAltIcon className="h-8 w-8 lg:h-12 lg:w-12 text-yellow-500" />
                                 <p className="text-xs lg:text-sm">UV Sterilizers</p>
