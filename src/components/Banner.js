00a5aa274d033324f5d33262e0e7f13929fb0fe1
tomboloski <ndanumumo93@gmail.com>
2023-06-28 02:07:17 +0300

    day 16

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -3,10 +3,8 @@ import { Carousel } from 'react-responsive-carousel';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import Image from 'next/image';
 import AOS from 'aos';
-import 'aos/dist/aos.css';
-import Banner4 from './Myimages/banner4.jpg';
-import Kioko from './Myimages/kioko.jpg';
 import Kioko5 from './Myimages/kioko5.jpg';
+import Newbanner5 from './Myimages/newbanner3.jpg';
 
 function Banner() {
   useEffect(() => {
@@ -14,7 +12,7 @@ function Banner() {
   }, []);
 
   return (
-    <div className="relative lg:mt-8 mt-24">
+    <div className="relative lg:mt-36">
       <div className="absolute w-full h-auto" />
       <Carousel
         autoPlay={true}
@@ -24,8 +22,16 @@ function Banner() {
         showThumbs={false}
         interval={3000}
       >
-        <div>
-          <Image src={Kioko5} loading="lazy" className="w-full h-auto" alt="Banner" />
+        <div className="mt-20">
+          <div className="h-[110px]">
+            <Image src={Kioko5} loading="lazy" className="w-full h-full object-cover" alt="Banner" />
+          </div>
+        </div>
+
+        <div className="mt-8">
+          <div className="h-[110px]">
+            <Image src={Newbanner5} loading="lazy" className="w-full h-full object-cover" alt="Banner" />
+          </div>
         </div>
       </Carousel>
     </div>
@@ -33,4 +39,5 @@ function Banner() {
 }
 
 export default Banner;
+
 
