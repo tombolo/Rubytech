94dbea404d651c5122f11f7e0854b7409ecd3317
tomboloski <ndanumumo93@gmail.com>
2023-06-28 04:43:53 +0300

    day 16

--- src/pages/product/[id].js
+++ src/pages/product/[id].js
@@ -89,7 +89,7 @@ const Product = ({title, price, description, category, image }) => {
 
     <div className="lg:flex lg:flex-row lg:mt-36 mt-28">
       {/*Product Part*/}
-                <div className="relative m-2 bg-white z-20 p-2 rounded-md shadow-lg md:w-1/2 border-t-2 border-yellow-900">
+                <div className="relative m-2 bg-white z-20 p-2 rounded-md shadow-lg md:w-1/2 border-2 border-yellow-900">
                             <p className="absolute top-2 right-2 text-gray-400 text-xs">{product.category}</p>
 
                                   <div className="relative overflow-hidden top-1 flex items-center justify-center rounded-md md:flex md:justify-start ">
