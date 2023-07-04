ccd4a8820b8f5567b4b0f42a9078bf526c9bcaf0
tomboloski <ndanumumo93@gmail.com>
2023-07-04 04:18:05 +0300

    day 20

--- src/components/About.js
+++ src/components/About.js
@@ -1,27 +1,32 @@
 import { LocationMarkerIcon, PhoneIcon, MailIcon } from '@heroicons/react/solid';
 import Banner2 from '../components/Myimages/banner2.jpg';
+import Image from 'next/image';
+import Blog3 from '../components/Myimages/blog3.jpg';
 
 
 function About() {
   return (
 
-    <div className="flex flex-col lg:mx-6 mx-2 bg-transparent rounded-md mt-4 lg:flex-row lg:items-center lg:justify-between flex-grow">
+    <div className="flex flex-col lg:mx-6 mx-2 rounded-md mt-4 lg:flex-row lg:items-center lg:justify-evenly bg-white flex-grow">
 
         <div  
-           className=" m-2 border-b-2 border-gray-200 lg:w-1/4 lg:justify-center lg:border-b-0">
+           className=" m-2 border-gray-200 lg:w-1/4 lg:justify-center lg:border-b-0">
             <h1 className="flex lg:justify-center text-sm font-bold text-blue-900 p-1">About Us</h1>
             <p className=" flex lg:justify-center text-xs p-1 lg:items-center">Rubytech Limited is a dealer of Cooking/Salad Oil ATMs, Milk ATMs, Water Purifiers, Food Grade Pumps, Automatic Bell Systems, Programmable Logic Controller, and Water Stations based in Buruburu Kenya.</p>
         </div>
 
-        <div className="m-2 border-b-2 border-gray-200 lg:w-1/4 lg:border-b-0">
-            <h1 className=" flex lg:justify-center text-sm font-bold text-blue-900 p-1">Quick Links</h1>
-            <p className="text-xs p-1 flex lg:justify-center">Home</p>
-            <p className="text-xs p-1 flex lg:justify-center">Our Services</p>
-            <p className="text-xs p-1 flex lg:justify-center">Electronics</p>
-            <p className="text-xs p-1 flex lg:justify-center">Buy Again</p>
+        <div className="m-2 lg:w-1/4 lg:border-b-0 md:hidden">
+        <div className="relative group rounded-md">
+            <Image src={Blog3} className="w-full h-full rounded-md" alt="Banner" />
+            <div className="absolute inset-0 rounded-md">
+            <div className="absolute inset-0 bg-gradient-to-b from-transparent to-white opacity-120 rounded-md"></div>
+            </div>
+        </div>
         </div>
 
-        <div className="m-2 border-b-2 border-gray-200 lg:w-1/4 lg:border-b-0">
+
+
+        <div className="m-2 border-b-2 border-gray-200 lg:w-1/4 lg:border-b-0 -mt-7 z-50 md:mt-0">
             <h1 className=" font-bold text-blue-900 p-1 flex lg:justify-center">Our Location & Contact Details</h1>
             <div className=" flex lg:justify-center flex-row p-1">
                 <div className="bg-blue-300 rounded-full">
